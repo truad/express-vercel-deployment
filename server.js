adc5fe2189b3b094ca5ed821b7002c57efc03e21
Truadpon Satayarak <truad.inventio@gmail.com>
2025-08-07 09:44:36 +0700

    adding status api

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('This is a landing page');
 });
 
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'Server is running' });
+});
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
